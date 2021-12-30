f7eb9396f106ffa588ddd92146c15df715a5ac91
lumin0u <lumin00u@gmail.com>
2021-12-30 17:26:26 +0100

    removing collisions behaviour and lake example

--- engine/model_.py
+++ engine/model_.py
@@ -1,21 +1,14 @@
 import json
-from enum import Enum
 
-from pygame import Vector2
 from engine.model.objloader import ObjLoader
 from helpers.dotenv import get_env
-
-
-class Behaviour(Enum):
-    VOID = 0
-    COLLIDEABLE = 1
+from helpers.vector import Vector2
 
 
 class Model:
-    def __init__(self, polygon, hitbox, behaviour):
+    def __init__(self, polygon, hitbox):
         self._polygon = polygon
         self._hitbox = hitbox
-        self._behaviour = behaviour
     
     @property
     def polygon(self):
@@ -25,14 +18,9 @@ class Model:
     def hitbox(self):
         return self._hitbox
     
-    @property
-    def behaviour(self):
-        return self._behaviour
-    
     @staticmethod
     def load(name):
         j_hitbox = json.loads(get_env("MODELS_DIR") + name + "/hitbox.json.json")
-        behaviour = Behaviour[j_hitbox["type"]]
         hitbox = [Vector2(**p) for p in j_hitbox["points"]]
         polygon = ObjLoader.load(get_env("MODELS_DIR") + name + "/" + name).get_polygon()
-        return Model(polygon, hitbox, behaviour)
+        return Model(polygon, hitbox)
