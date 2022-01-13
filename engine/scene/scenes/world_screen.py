a478c2e9076575c19abbee9f11c48c50b844a4d1
lumin0u <lumin00u@gmail.com>
2022-01-13 20:21:35 +0100

    ptit improve

--- engine/scene/scenes/world_screen.py
+++ engine/scene/scenes/world_screen.py
@@ -1,12 +1,15 @@
-from engine.scene.scene import Scene
 from abc import ABC
+
+from engine.scene.scene import Scene
 from world.world import World
 
 
 class WorldScreen(Scene, ABC):
     world = None
+    
     def __init__(self, root):
         super().__init__(root)
 
+
 def get_world_screen_type(world: World):
-    return type("_WorldScreen", (WorldScreen,), {"world": world})
+    return type("WorldScreen_Impl", (WorldScreen,), {"world": world})
