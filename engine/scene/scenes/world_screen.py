0e1838c9d9970f0b22a85b2f1b850345732d6f9f
Mattéo <40558258+Matteo0810@users.noreply.github.com>
2022-01-17 21:04:23 +0100

    improving user interface

--- engine/scene/scenes/world_screen.py
+++ engine/scene/scenes/world_screen.py
@@ -2,10 +2,14 @@ from engine.scene.scene import Scene
 
 
 class WorldScreen(Scene):
-    
+
     def __init__(self, root):
         super().__init__(root)
-    
+        self._set_ground()
+
+    def _set_ground(self):
+        self.get_models().add('ground/ground', size=10)
+
     @staticmethod
     def with_(world):
         return type("WorldScreen_Impl", (WorldScreen,), {"world": world})
--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-from helpers.dotenv import dotenv, get_env
+from helpers.dotenv import dotenv
 from engine.scene.frame import Frame
 
 if __name__ == "__main__":
     dotenv()
 
     frame = Frame()
-
-    # json_world = json.loads(open("world/assets/forest.json", mode='r').read())
-    # world = World.load(json_world)
-
-    scene = frame.get_scene()
-
-    # modelA = models.add(f'{get_env("MODELS_DIR")}/cliff/cliff')
-    # road = RoadModel([10, 10], [100, 100], models)
-
     frame.show()
